36785eb2c9632f28776d445a471cabf0a428fbb3
Fernandomtos <rmavali@bol.com.br>
2023-08-09 18:06:21 -0300

    fix: recover Password

--- src/services/recoverPass/recoverPassword.service.ts
+++ src/services/recoverPass/recoverPassword.service.ts
@@ -9,20 +9,41 @@ const recoverPasswordService = async (
   userEmail: TUserEmailData,
   userId: number
 ): Promise<void> => {
+
+  // CONFIGURAÇÃO PARA MAILTRAP
+  // const transport = nodemailer.createTransport({
+  //   host: "smtp.sendgrid.net",
+  //   port: 587,
+  //   auth: {
+  //     user: "19fb67f0acc908",
+  //     pass: "a0c52b0192dfb4",
+  //   },
+  // });
+
+  //CONFIGURAÇÃO PARA GMAIL
+
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
   const transport = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
+    service: 'gmail',
     auth: {
-      user: "19fb67f0acc908",
-      pass: "a0c52b0192dfb4",
+      user: 'emailapiprojecttest@gmail.com',
+      pass: 'gbvxagpnqrqgnxxc',
     },
   });
+
+  transport.verify()
+    .then(console.log)
+    .catch(() => {
+      throw new AppError("Fail to send mail", 404);
+    });
 
   const newPassword = crypto.randomBytes(4).toString("hex");
 
   transport
     .sendMail({
-      from: "Adminstrador <7aa89a43bc-1cb999@inbox.mailtrap.io>",
+      // from: "Adminstrador <7aa89a43bc-1cb999@inbox.mailtrap.io>",
+      from: "Adminstrador <emailapiprojecttest@gmail.com>",
       to: userEmail.email,
       subject: "Recuperação de senha",
       html: `<p>Olá, segue senha de recuperação: ${newPassword}</p><br/><a href="http://localhost:3000/login">Página Login</a>`,
